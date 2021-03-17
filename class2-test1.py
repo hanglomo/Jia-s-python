fb948f997fa9e1300003ad5896b5c6b1de807707
hanglomo <shhlomo@Outlook.com>
2021-03-17 09:46:43 +0800

    Update class2-test1.py
    update

--- class2-test1.py
+++ class2-test1.py
@@ -24,3 +24,26 @@
     print(y)
 
     import turtle as t
+    t.shape('turtle')
+    t.shape('arrow')
+    t.shape('pig')
+    t.fd(50)
+    t.pensize(15)
+
+    import turtle as t
+    t.color("yellow")
+    t.pensize(10)
+    t.fd(60)
+    t.pensize(7)
+    t.color("red")
+    t.fd(150)
+     t.color("yellow")
+    t.pensize(10)
+    t.fd(60)
+
+
+    
+
+
+
+
