3acb6a1cbdbe28399b4533025993531bddedc625
hanglomo <shhlomo@Outlook.com>
2021-03-17 09:38:11 +0800

    Add files via upload
    贾耀云Python第二节课（运算符）课堂源代码

---
+++ class2-test1.py
@@ -0,0 +1,26 @@
+    print("1")
+
+    x=int(input("请输入 父亲的身高"))
+    y=int(input("请输入 母亲的身高"))
+    z=x+y
+    z=z*0.54
+
+
+    print(z)
+
+
+    x=int(input("请输入 杨树的数量"))
+    y=x*2
+    y=y+4
+    print("柳树的数量是:",y)
+
+    20==30
+    1==1
+
+    x=50/9
+    x=int(x)
+    print(x)
+    y=50%9
+    print(y)
+
+    import turtle as t
